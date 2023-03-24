3f233cac4eae323943393a6a4a011255361f50db
dimyG <dimgeows@gmail.com>
2023-03-24 21:30:46 +0200

    using a text index to search

--- main.py
+++ main.py
@@ -31,6 +31,25 @@ logger.debug(f"mongo_collection_name: {mongo_collection_name}")
 app = FastAPI()
 
 
+async def count_indexes(collection):
+    count = 0
+    async for _ in collection.list_indexes():
+        count += 1
+    return count
+
+
+async def load_data_from_file(file_path="mongo_initial_data/garments.jl"):
+    logger.info(f"Loading data from file {file_path}...")
+    garments = []
+    with open(file_path, "r") as f:
+        for line in f:
+            garment = json.loads(line)  # Convert JSON string to Python dict
+            garments.append(garment)
+
+    # Bulk insert the garments into the MongoDB collection
+    await app.mongodb[mongo_collection_name].insert_many(garments)
+
+
 # Register a startup event handler function
 @app.on_event("startup")
 async def startup_db_client():
@@ -38,17 +57,19 @@ async def startup_db_client():
     app.mongodb_client = AsyncIOMotorClient(mongo_url)
     app.mongodb = app.mongodb_client[mongo_db]
 
-    # if mongo has no data, load data from a jl file
+    # if mongo has no data, load data from the jl file
     if await app.mongodb[mongo_collection_name].count_documents({}) == 0:
-        logger.info("Loading data from file...")
-        garments = []
-        with open("mongo_initial_data/garments.jl", "r") as f:
-            for line in f:
-                garment = json.loads(line)  # Convert JSON string to Python dict
-                garments.append(garment)
+        await load_data_from_file()
 
-        # Bulk insert the garments into the MongoDB collection
-        await app.mongodb[mongo_collection_name].insert_many(garments)
+    index_count = await count_indexes(app.mongodb[mongo_collection_name])
+    logger.debug(f"index_count: {index_count}")
+
+    # if garments collection has only the default index, create one on the product_title and product_description fields
+    if index_count == 1:
+        logger.info("Creating text index...")
+        await app.mongodb[mongo_collection_name].create_index(
+            [("product_title", "text"), ("product_description", "text")]
+        )
 
 
 # Register a shutdown event handler function
@@ -63,17 +84,28 @@ async def shutdown_db_client():
 async def search_garments(query: str):
     garments = []
 
+    projection = {"position": 0, "product_imgs_src": 0, "image_urls": 0}
+
+    # search based on text index
     cursor = app.mongodb[mongo_collection_name].find(
-        {"product_title": {"$regex": f".*{query}.*", "$options": "i"}}
+        {"$text": {"$search": query}},  # Search for garments that match the query based on the text index
+        projection=projection  # Use projection to fetch only required fields
     )
 
+    # search based on regex
+    # cursor = app.mongodb[mongo_collection_name].find(
+    #     # use a regex to search for garments that match the query
+    #     {"$or": [{"product_title": {"$regex": query, "$options": "i"}}, {"product_description": {"$regex": query, "$options": "i"}}]},
+    #     projection=projection  # Use projection to fetch only required fields
+    # )
+
     async for garment in cursor:
-        # Convert the ObjectId to a string before appending the garment
+        # ObjectId is not json serializable, convert it to a string before appending the garment
         garment["_id"] = str(garment["_id"])
         garments.append(garment)
-        logger.debug(f"garment: {garment['product_title']}")
+        logger.debug(f"garment: {garment}")
 
-    logger.debug(f"returning {len(garments)} garments...")
+    logger.debug(f"{len(garments)} garments found.")
 
-    return garments[:10]
+    return garments[:20]
 
