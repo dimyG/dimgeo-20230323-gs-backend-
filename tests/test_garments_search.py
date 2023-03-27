2de4423fbd508f4b553c4eea099c57528bb2adc3
dimyG <dimgeows@gmail.com>
2023-03-27 15:49:33 +0300

    use query parameter in tests

--- tests/test_garments_search.py
+++ tests/test_garments_search.py
@@ -4,11 +4,12 @@
 
 default_skip = 0
 default_limit = 20
+query = "white hat"
 
 
 def test_search_garments(client):
     # check that a query returns results
-    response = client.get("/search", params={"query": "shirt", "skip": 0, "limit": 10})
+    response = client.get("/search", params={"query": query, "skip": 0, "limit": 10})
     assert response.status_code == 200
     data = response.json()
     assert len(data) <= 10
@@ -16,11 +17,11 @@ def test_search_garments(client):
 
 def test_search_garments_different_skip(client):
     # check that the same query but with different skip returns different results
-    response = client.get("/search", params={"query": "shirt", "skip": 0, "limit": 10})
+    response = client.get("/search", params={"query": query, "skip": 0, "limit": 10})
     assert response.status_code == 200
     data = response.json()
 
-    response = client.get("/search", params={"query": "shirt", "skip": 10, "limit": 10})
+    response = client.get("/search", params={"query": query, "skip": 10, "limit": 10})
     assert response.status_code == 200
     data3 = response.json()
 
@@ -42,14 +43,14 @@ def test_search_garments_no_query(client):
 
 
 def test_search_garments_no_skip_limit(client):
-    response = client.get("/search", params={"query": "shirt"})
+    response = client.get("/search", params={"query": query})
     assert response.status_code == 200
     data = response.json()
     assert len(data) <= default_limit
 
 
 def test_search_garments_invalid_params(client):
-    response = client.get("/search", params={"query": "shirt", "skip": "invalid", "limit": "invalid"})
+    response = client.get("/search", params={"query": query, "skip": "invalid", "limit": "invalid"})
     assert response.status_code == 422
     data = response.json()
     assert data["detail"][0]["type"] == "type_error.integer"
@@ -57,14 +58,14 @@ def test_search_garments_invalid_params(client):
 
 
 def test_search_garments_no_skip(client):
-    response = client.get("/search", params={"query": "shirt", "limit": 10})
+    response = client.get("/search", params={"query": query, "limit": 10})
     assert response.status_code == 200
     data = response.json()
     assert len(data) <= 10
 
 
 def test_search_garments_no_limit(client):
-    response = client.get("/search", params={"query": "shirt", "skip": 0})
+    response = client.get("/search", params={"query": query, "skip": 0})
     assert response.status_code == 200
     data = response.json()
     assert len(data) <= default_limit
